2478ee2de5c506dcfdd2cb7daef5ad2fdcf064c3
Oren Lev <orenslev@gmail.com>
2022-03-04 20:05:46 +0200

    Initial Push

---
+++ Form1.cs
@@ -0,0 +1,280 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CreationDate
+{
+	/// <summary>
+	/// Summary description for Form1.
+	/// </summary>
+	public class Form1 : System.Windows.Forms.Form
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.Container components = null;
+		private Hashtable m_FilesToRename = new Hashtable();
+		private System.Windows.Forms.ProgressBar m_RenameProgressBar;
+		private ArrayList m_CommandLinePath = new ArrayList();
+		private bool m_FirstIdle = true;
+		private string m_CurPath;
+
+		public Form1()
+		{
+			//
+			// Required for Windows Form Designer support
+			//
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if (components != null) 
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(Form1));
+			this.m_RenameProgressBar = new System.Windows.Forms.ProgressBar();
+			this.SuspendLayout();
+			// 
+			// m_RenameProgressBar
+			// 
+			this.m_RenameProgressBar.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.m_RenameProgressBar.Location = new System.Drawing.Point(0, 0);
+			this.m_RenameProgressBar.Name = "m_RenameProgressBar";
+			this.m_RenameProgressBar.Size = new System.Drawing.Size(426, 24);
+			this.m_RenameProgressBar.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(426, 24);
+			this.Controls.Add(this.m_RenameProgressBar);
+			this.Cursor = System.Windows.Forms.Cursors.Arrow;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Processing...";
+			Application.Idle += new System.EventHandler(this.OnIdle);
+			this.ResumeLayout(false);
+
+		}
+		#endregion
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main() 
+		{
+			Application.Run(new Form1());
+		}
+		
+		public void FillRenameHashTable()
+		{
+			Application.DoEvents();
+
+			m_CurPath = Convert.ToString(m_CommandLinePath[1]);
+			m_CurPath = m_CurPath.TrimEnd('\\');
+			m_CurPath = m_CurPath.TrimEnd('/');
+			
+			FileInfo curFileInfo = new FileInfo(m_CurPath);
+
+			if(Convert.ToBoolean(curFileInfo.Attributes & FileAttributes.Directory))
+            {
+				//This is a directory, get the list of file paths
+				DirectoryInfo curDirectoryInfo = new DirectoryInfo(m_CurPath);
+				FileInfo[] fileInfoArray = curDirectoryInfo.GetFiles();
+
+				m_RenameProgressBar.Maximum = 2*fileInfoArray.Length;
+
+				for(int j=0; j<fileInfoArray.Length; j++){
+
+					Application.DoEvents();
+
+					FileInfo curInfo = fileInfoArray[j];
+					SaveNewPaths(curInfo.FullName);
+				}
+			}
+			else
+            {
+				SaveNewPaths(Convert.ToString(m_CommandLinePath[1]));
+			}
+		}
+		
+		public void GetCommandLinePath()
+		{
+			m_CommandLinePath.AddRange(Environment.GetCommandLineArgs());
+			m_RenameProgressBar.Minimum = 0;
+			m_RenameProgressBar.Maximum = 2*(m_CommandLinePath.Count - 1);
+		}
+
+		public void SaveNewPaths(string imageFullPath)
+		{
+			string newFileName = "";
+
+			string imageFileName = Path.GetFileName(imageFullPath);
+			string imagePath = imageFullPath.Substring(0, imageFullPath.Length - imageFileName.Length);
+
+			string ext = Path.GetExtension(imageFullPath);
+			ext	= ext.ToLower();
+
+            if (ext.CompareTo(".jpg") == 0 || ext.CompareTo(".png") == 0 ||
+                ext.CompareTo(".m2ts") == 0 || ext.CompareTo(".mts") == 0 ||
+                ext.CompareTo(".mp4") == 0)
+            {
+                try
+                {
+                    // Create an Image object from the specified file.
+                    Image img = Image.FromFile(imageFullPath, true);
+
+                    System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+
+                    PropertyItem propItem = img.GetPropertyItem(0x9003/*'DateTimeOriginal' field from EXIF*/);
+					newFileName = encoding.GetString(propItem.Value, 0, propItem.Len - 1);
+				}
+				// An invalid image will throw an OutOfMemoryException 
+				// exception (for example a .mov file)
+				catch (Exception) 
+                {
+                    if (ext.CompareTo(".m2ts") == 0 || ext.CompareTo(".mts") == 0 ||
+                        ext.CompareTo(".mp4") == 0 || ext.CompareTo(".png") == 0)
+					{
+						FileInfo nonImageInfo = new FileInfo(imageFullPath);
+
+                        if (new FileInfo(imageFullPath + ".modd").Exists)
+                        {
+                            newFileName = Path.GetFileNameWithoutExtension(nonImageInfo.Name.Insert(8, "_"));
+                        }
+                        else if (ext.CompareTo(".mp4") == 0 || ext.CompareTo(".png") == 0)
+                        {
+                            newFileName = Path.GetFileNameWithoutExtension(nonImageInfo.Name);
+                        }
+                        else
+                        {
+                            DateTime creationTime = nonImageInfo.CreationTime;
+                            newFileName = creationTime.ToString("yyyyMMdd_HHmmss");
+                        }
+					}
+                    else if (ext.CompareTo(".jpg") == 0)
+                    {
+                        newFileName = Path.GetFileNameWithoutExtension(imageFileName);
+                    }
+				}
+
+				if(newFileName.Length > 0)
+				{
+                    newFileName = newFileName.Replace("-", "");
+                    newFileName = newFileName.Replace(".", "");
+					newFileName	= newFileName.Replace(":", "");
+					newFileName	= newFileName.Replace(" ", "_");
+                    newFileName = newFileName.Replace("VID_", "");
+					newFileName = newFileName.Replace("PIX_", "");
+
+					m_FilesToRename.Add(imageFullPath, newFileName + ext);
+					this.Text = "Creating new name: '" + newFileName + ext + "'";
+				}
+
+				newFileName = "";
+
+				m_RenameProgressBar.Increment(1);
+			}
+		}
+
+		public void RenameFilesInList()
+		{
+            List<string> filesToRenameSorted = new List<string>(m_FilesToRename.Count);
+ 
+            foreach(object key in m_FilesToRename.Keys)
+            {
+                filesToRenameSorted.Add(Convert.ToString(key));
+            }
+
+            filesToRenameSorted.Sort();
+
+			m_CurPath += "/";
+            string prevSaveFileName = string.Empty;
+            int index = 0;
+
+            foreach(string fileToRenamePath in filesToRenameSorted)
+            {
+				try
+                {
+                    FileInfo curImageInfo = new FileInfo(fileToRenamePath);
+
+                    string saveFileName = Convert.ToString(m_FilesToRename[fileToRenamePath]);
+
+                    if (saveFileName == prevSaveFileName)
+                    {
+                        saveFileName = String.Format("{0}_{1}{2}", 
+                            Path.GetFileNameWithoutExtension(saveFileName),
+                            index++,
+                            Path.GetExtension(saveFileName));
+                    }
+                    else
+                    {
+                        index = 0;
+                        prevSaveFileName = saveFileName;
+                    }
+
+                    string newFileName = saveFileName;
+
+					this.Text = "Renaming '" + curImageInfo.Name + "' to: '" + newFileName + "'";
+
+					Application.DoEvents();
+
+					curImageInfo.CopyTo(m_CurPath + newFileName);
+					curImageInfo.Delete();
+					
+					m_RenameProgressBar.Increment(1);
+				}
+				catch (Exception) {}
+			}
+		}
+
+		private void OnIdle(object sender, System.EventArgs e)
+		{
+			if(m_FirstIdle)
+            {
+				m_FirstIdle = false;
+
+				GetCommandLinePath();
+
+				FillRenameHashTable();
+
+				//Do a garbage collection, otherwise we cannot delete the files
+				//after we copy them to a new name.
+				GC.Collect();
+
+				RenameFilesInList();
+
+				this.Close();
+			}
+		}
+	}
+}
